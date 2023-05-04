4832f89a1bf68a04d6639b6e9a74ee3cde03cc9b
woosung yang <ngxk1210@gmail.com>
2023-05-04 13:17:08 +0900

    5월_4일_자바_연습_자료형_연산자

---
+++ src/test00001/test0101.java
@@ -0,0 +1,45 @@
+package test00001;
+
+public class test0101 {
+	
+	public static void main(String[] args) {
+		
+		String fname = "고길동";
+		String mname = "고길순";
+		String name3 = "고하루";
+		String myname = "고하늘";
+				
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		System.out.println();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+
+}
