07fc85df9f0d66f04fef31f003ed49e4a1828a4e
woosung yang <ngxk1210@gmail.com>
2023-05-26 17:40:36 +0900

    5월 23일자 작업-3

--- src/chapter10/ex07/Ex01.java
+++ src/chapter10/ex07/Ex01.java
@@ -42,7 +42,8 @@ public class Ex01 {
 	//배열의 객체를 매개변수로 받아서 총 가격을 출력
 	void totalPrice(Object[] obj) {     //obj = {a1, b1, s1} 
 		
-		int sumApple = 0 ; 
+		/*합계를 구하는 변수 선언
+		int sumApple = 0 ; 		//price*count
 		sumApple =  ((Apple) obj[0]).price * ((Apple) obj[0]).count; 
 		int sumBanana = 0 ; 
 		sumBanana =  ((Banana) obj[1]).price * ((Banana) obj[1]).count; 
@@ -52,10 +53,31 @@ public class Ex01 {
 		int sum =0;
 		sum = sumApple + sumBanana + sumStrawberry;
 		
-		System.out.printf("애플의 합은 %d 입니다. \n", sumApple);
+		System.out.printf("사과의 합은 %d 입니다. \n", sumApple);
 		System.out.printf("바나나의 합은 %d 입니다. \n", sumBanana);
 		System.out.printf("딸기의 합은 %d 입니다. \n", sumStrawberry);
 		System.out.println("전체 과일의 총합은 " + sum + " 입니다.");
+		
+		 */
+		
+		//배열 변수로 선언
+		int[] sum = new int[3];		//sum[0]:apple, sum[1] : banana, sum[2] : strawberry
+		
+		//다운 캐스팅
+		if(obj[0] instanceof Apple) {
+			sum[0] = ((Apple) obj[0]).price * ((Apple) obj[0]).count; 
+		}
+		if(obj[1] instanceof Banana) {
+			sum[1] = ((Banana) obj[1]).price * ((Banana) obj[1]).count; 
+		}
+		if(obj[2] instanceof Strawberry) {
+			sum[2] =  ((Strawberry) obj[2]).price * ((Strawberry) obj[2]).count; 
+		}
+		
+		System.out.println("사과의 합은 : " + sum[0]);
+		System.out.println("바나나의 합은 : " + sum[1]);
+		System.out.println("딸기의 합은 합은 : " + sum[2]);
+		System.out.println("전체 과일의 총합은 " + (sum[0] + sum[1] + sum[2]));
 		
 	}
 	
@@ -75,14 +97,20 @@ public class Ex01 {
 		Banana b1 = new Banana("바나나", 4000, 10); 
 		Strawberry s1 = new Strawberry("딸기", 5000, 30); 
 		
+		//배열 선언후 값 할당
+		
+		Object[]obj = new Object[] {a1, b1, s1};
+		//Object[]arr = new Object[] {a1, b1, s1};
+		//a1 : Object/apple
+		//b1 : Object/banana
+		//s1 : Object/strawberry
 		
 		//모든 객체를 Object 배열 안에 넣는다.
 		
-		Object[]obj = new Object[] {a1, b1, s1};
-		
-		//
+		//메소드 호출
 		Ex01 e = new Ex01(); 
 		e.totalPrice(obj);
+		//e.totalPrice(arr);
 	
 	}
 
