141a90c0b09adfd7fbb0836f236a8d1eae5781cf
woosung yang <ngxk1210@gmail.com>
2023-06-08 09:17:37 +0900

    6월 8일

--- src/Chapter17/ex05/Ex01.java
+++ src/Chapter17/ex05/Ex01.java
@@ -2,11 +2,41 @@ package Chapter17.ex05;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Objects;
 
-class Student{//애가 map : key : 중복되면 안됨, equals(), hashcode()
+class Student {   // Map : Key  : 중복되면 안됨 , equals(), hashCode() 
 	
-	int stuID;		//중복되면 안되도록 처리,
-	String stuName;
+	int stuID ;        // 중복되면 안되도록 처리, 
+	String stuName; 	
+	
+	Student(int stuID, String stuName){
+		this.stuID = stuID; 
+		this.stuName = stuName; 
+	}
+	
+	@Override
+		public String toString() {
+			return  "" + stuID + "";
+		}
+
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(stuID);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		return stuID == other.stuID;
+	}
+	
 	
 }
 
@@ -14,19 +44,19 @@ class Student{//애가 map : key : 중복되면 안됨, equals(), hashcode()
 public class Ex01 {
 	public static void main(String[] args) {
 		
-		//Key : Student , Value
-		Map<Student, String>linkMap = new LinkedHashMap();
-		linkMap.put(1111, "홍길동");
-		linkMap.put(1122, "홍길동");
-		linkMap.put(1123, "홍길동");
-		linkMap.put(1131, "홍길동");
+		//Key : Student , : 6시 : 20분까지 : p.jangwoo@gmail.com 
+		Map <Student , String >  linkMap = new LinkedHashMap(); 
+		
+		linkMap.put(new Student(1111,"홍길동"), "학생 - 홍길동"); 
+		linkMap.put(new Student(2222,"김길동"), "학생 - 김길동"); 
+		linkMap.put(new Student(5555,"이길동"), "학생 - 이길동"); 
+		linkMap.put(new Student(3333,"옥길동"), "학생 - 옥길동"); 
+		linkMap.put(new Student(1111,"중복 길동"), "학생 - 홍길동");    //중복저장 안됨
+		linkMap.put(new Student(1111,"중복 길동"), "학생 - 홍길동");    //중복저장 안됨 
 		
 		
 		System.out.println(linkMap);
-		
-		
-		
-		
+			// 출력 정보 : {1111=학생 - 홍길동, 2222=학생 - 김길동, 5555=학생 - 이길동, 3333=학생 - 옥길동}
 		
 		
 	}
